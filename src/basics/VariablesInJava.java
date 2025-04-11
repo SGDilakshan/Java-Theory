3914eb86eefa2327ab6a4af7d09b9388125aca85
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-11 20:35:21 +0530

    Update the Variables in Java

---
+++ src/basics/VariablesInJava.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class VariablesInJava {
+    public static void main(String[] args) {
+        String name = "Dilakshan";
+        int age = 25;
+        float percentage = 25.25f;
+        char gender = 'M';
+        boolean married = true;
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Percentage: "+percentage);
+        System.out.println("Gender: "+gender);
+        System.out.println("Married: "+married);
+    }
+}
