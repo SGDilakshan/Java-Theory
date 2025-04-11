3432589dbb04c18bcc8d7fe17ed22a9ad668306a
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-11 16:18:26 +0530

    Update the Single and Multi Line Comments in Java

---
+++ src/basics/SingleAndMultiLineComments.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class SingleAndMultiLineComments {
+    public static void main(String[] args) {
+        // THIS IS SINGLE LINE COMMENTS
+        System.out.println("Hi");
+        /*
+        THIS PART IS ALLOCATED FOR MULTI LINE COMMENTS
+        */
+
+    }
+}
