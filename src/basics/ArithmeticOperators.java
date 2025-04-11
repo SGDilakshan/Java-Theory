580b4649663f589ee5ef9d342fc77e0fb66ae3c6
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-11 20:41:28 +0530

    Update the Arithmetic Operators in Java

---
+++ src/basics/ArithmeticOperators.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 123,b=10;
+        System.out.println("Addition: "+(a+b));
+        System.out.println("Subtraction: "+(a-b));
+        System.out.println("Multiplication: "+(a*b));
+        System.out.println("Division: "+(a/b));
+        System.out.println("Modulus: "+(a%b));
+    }
+}
