58a8d3c3491a0dd62b0c6fcfe1642b653963afa3
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-11 21:30:39 +0530

    Update the Conditional Operators in Java

---
+++ src/basics/ConditionalOperators.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class ConditionalOperators {
+    public static void main(String[] args) {
+        // Conditional Operator or Ternary Operator in Java
+        int a=45,b=35,c;
+        c=a>b?a:b;
+        System.out.println("The Greatest Number is : "+c);
+    }
+}
