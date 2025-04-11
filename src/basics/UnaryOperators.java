a32c3bd36b27f4a5caeaa322cbf63b2c1d3db9d0
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-11 21:31:40 +0530

    Update the Unary Operators in Java

---
+++ src/basics/UnaryOperators.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class UnaryOperators {
+    public static void main(String[] args) {
+        // Unary Operator in Java ++ --
+        int a=10;
+        System.out.println(a); //10
+        a++;
+        System.out.println(a); //11
+        System.out.println(a++); //11
+        System.out.println(a); //12
+        System.out.println(++a); //13
+        System.out.println(a); //13
+        System.out.println(a--); //13
+        System.out.println(a); //12
+        System.out.println(--a); //11
+        System.out.println(a); //11
+    }
+}
