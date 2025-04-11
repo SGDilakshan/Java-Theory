f38344867887b1e4e639300db3db736c75d3e385
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-11 21:25:56 +0530

    Update the Arithmetic Assignment Operators in Java

---
+++ src/basics/ArithmeticAssignmentOperators.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class ArithmeticAssignmentOperators {
+    public static void main(String[] args) {
+        int a=123;
+        System.out.println(a);
+        a+=10; // a=a+10;
+        System.out.println(a);
+        a-=10;
+        System.out.println(a);
+        a*=10;
+        System.out.println(a);
+        a/=10;
+        System.out.println(a);
+        a%=10;
+        System.out.println(a);
+    }
+}
