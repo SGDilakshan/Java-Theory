21d2662c0befc834cf7afb531d0e5f4ff3c00794
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-11 21:33:16 +0530

    Update the Bitwise and Shift Operators in Java

---
+++ src/basics/BitwiseAndShiftOperators.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class BitwiseAndShiftOperators {
+    public static void main(String[] args) {
+        // Bitwise and Shift Operators in Java
+        int a=25,b=45;
+        System.out.println("Bitwise And : "+(a&b));
+        System.out.println("Bitwise Or : "+(a|b));
+        System.out.println("Bitwise Xor : "+(a^b));
+        System.out.println("Bitwise Not : "+(~a));
+    }
+}
