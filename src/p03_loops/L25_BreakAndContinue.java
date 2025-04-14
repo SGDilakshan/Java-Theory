7bbe56e7b3ef74c7672bba9f82df6f8cd46ca59c
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-14 14:43:00 +0530

    Update the BreakAndContinue in Java

---
+++ src/p03_loops/L25_BreakAndContinue.java
@@ -0,0 +1,15 @@
+package p03_loops;
+
+public class L25_BreakAndContinue {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+            if (i == 8) {
+                break;
+            }
+        }
+    }
+}
