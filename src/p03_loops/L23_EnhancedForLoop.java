110db0711139e6a825afd950e205b9e0a574833d
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-14 14:39:48 +0530

    Update the EnhancedForLoop in Java

---
+++ src/p03_loops/L23_EnhancedForLoop.java
@@ -0,0 +1,10 @@
+package p03_loops;
+
+public class L23_EnhancedForLoop {
+    public static void main(String[] args) {
+        int numbers[]={10,20,30,40,50,60,70};
+        for(int n:numbers){
+            System.out.println(n);
+        }
+    }
+}
