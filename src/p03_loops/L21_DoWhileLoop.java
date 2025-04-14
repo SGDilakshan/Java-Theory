4f3f1537c66c55c7f3b4f4aeb1f4c8d192f2c759
SGDilakshan <sgd.dilakshan@gmail.com>
2025-04-14 14:30:48 +0530

    Update the DoWhileLoop in Java

---
+++ src/p03_loops/L21_DoWhileLoop.java
@@ -0,0 +1,24 @@
+package p03_loops;
+
+import java.util.Scanner;
+
+public class L21_DoWhileLoop {
+    public static void main(String[] args) {
+        System.out.println("Enter the limit: ");
+        Scanner scanner = new Scanner(System.in);
+        int limit = scanner.nextInt();
+        /*
+        int i=1;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i<=limit);
+         */
+
+        int i=2;
+        do{
+            System.out.println(i);
+            i+=2;
+        }while(i<=limit);
+    }
+}
